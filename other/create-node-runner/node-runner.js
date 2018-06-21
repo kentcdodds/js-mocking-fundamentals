ee891fbb364d4facb6020ea0c37784228b214a57
Kent C. Dodds <kent@doddsfamily.us>
2018-06-20 21:11:35 -0600

    add support for streaming stdout and stderr

--- other/create-node-runner/node-runner.js
+++ other/create-node-runner/node-runner.js
@@ -3,6 +3,9 @@ const {fail, pass} = require('create-jest-runner')
 
 module.exports = async ({testPath}) => {
   const worker = new Worker(require.resolve('./require-module'))
+  worker.getStdout().pipe(process.stdout)
+  worker.getStderr().pipe(process.stderr)
+  process.stdout.write('\n')
   const start = +new Date()
   try {
     await worker.require(testPath)
