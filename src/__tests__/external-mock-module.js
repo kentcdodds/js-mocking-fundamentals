523852cb0f8265d914d46f6c4c5215918572154f
MujunZ <o.6180339@gmail.com>
2020-07-21 21:43:51 -0700

    add exercise for external-mock-module

--- src/__tests__/external-mock-module.js
+++ src/__tests__/external-mock-module.js
@@ -1,16 +1,38 @@
+/**
+ * Often you’ll want to mock the same file throughout
+ * all the tests in your codebase. So let’s make a
+ * shared mock file in Jest's __mocks__ directory which
+ * Jest can load for us automatically.
+ *
+ * Task: refactor the code to use an external mock module.
+ *
+ * Execute: Use `npx jest --watch src/__tests__/external-mock-module.js` to watch the test
+ */
+
 const thumbWar = require('../thumb-war')
 const utilsMock = require('../utils')
 
-jest.mock('../utils')
+jest.mock('../utils', () => {
+  return {
+    getWinner: jest.fn((p1, p2) => p1),
+  }
+})
 
 test('returns winner', () => {
   const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
   expect(winner).toBe('Kent C. Dodds')
   expect(utilsMock.getWinner.mock.calls).toEqual([
     ['Kent C. Dodds', 'Ken Wheeler'],
-    ['Kent C. Dodds', 'Ken Wheeler']
+    ['Kent C. Dodds', 'Ken Wheeler'],
   ])
 
   // cleanup
   utilsMock.getWinner.mockReset()
 })
+
+/**
+ * Hints:
+ * - https://jestjs.io/docs/en/manual-mocks
+ *
+ * Checkout master branch to see the answer.
+ */
--- src/no-framework/external-mock-module.js
+++ src/no-framework/external-mock-module.js
@@ -1,7 +1,27 @@
-require('../__no-framework-mocks__/utils') // prime the cache
+/**
+ * Task: refactor the code to use an external mock module.
+ *
+ * Execute: Use `npx jest --watch src/no-framework/external-mock-module.js` to watch the test
+ */
+
+function fn(impl = () => {}) {
+  const mockFn = (...args) => {
+    mockFn.mock.calls.push(args)
+    return impl(...args)
+  }
+  mockFn.mock = {calls: []}
+  return mockFn
+}
+
 const utilsPath = require.resolve('../utils')
-const mockUtilsPath = require.resolve('../__no-framework-mocks__/utils')
-require.cache[utilsPath] = require.cache[mockUtilsPath]
+require.cache[utilsPath] = {
+  id: utilsPath,
+  filename: utilsPath,
+  loaded: true,
+  exports: {
+    getWinner: fn((p1, p2) => p1),
+  },
+}
 
 const assert = require('assert')
 const thumbWar = require('../thumb-war')
@@ -11,8 +31,12 @@ const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
 assert.strictEqual(winner, 'Kent C. Dodds')
 assert.deepStrictEqual(utils.getWinner.mock.calls, [
   ['Kent C. Dodds', 'Ken Wheeler'],
-  ['Kent C. Dodds', 'Ken Wheeler']
+  ['Kent C. Dodds', 'Ken Wheeler'],
 ])
 
 // cleanup
 delete require.cache[utilsPath]
+
+/**
+ * Checkout master branch to see the answer.
+ */
