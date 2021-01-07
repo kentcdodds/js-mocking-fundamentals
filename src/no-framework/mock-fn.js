97303ab61a42def8490aa894f23e2cdafb132558
Daniel Gih <11883444+danly@users.noreply.github.com>
2021-01-07 12:10:06 +0900

    Remove mockImplementation from mock-fn lesson (#7)

--- src/no-framework/mock-fn.js
+++ src/no-framework/mock-fn.js
@@ -2,13 +2,12 @@ const assert = require('assert')
 const thumbWar = require('../thumb-war')
 const utils = require('../utils')
 
-function fn(impl = () => {}) {
+function fn(impl) {
   const mockFn = (...args) => {
     mockFn.mock.calls.push(args)
     return impl(...args)
   }
   mockFn.mock = {calls: []}
-  mockFn.mockImplementation = newImpl => (impl = newImpl)
   return mockFn
 }
 
