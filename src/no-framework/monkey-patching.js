b4852fcf4e739f6a3d5e71d5f7b6123c36849867
edevars <e.devars.dev@gmail.com>
2021-07-03 17:39:27 -0500

    :sparkles: Monkey Patch

--- src/no-framework/monkey-patching.js
+++ src/no-framework/monkey-patching.js
@@ -23,14 +23,18 @@ const assert = require('assert')
 const thumbWar = require('../thumb-war')
 const utils = require('../utils')
 
+
 // Your code:
 // monkey patch
+const originalGetWinner = utils.getWinner
+utils.getWinner = (p1,p2) => p1
 
 const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
 assert.strictEqual(winner, 'Kent C. Dodds')
 
 // Your code:
 // cleanup
+utils.getWinner = originalGetWinner
 
 /**
  * Checkout master branch to see the answer.
